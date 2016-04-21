8f22609450237486aa5a1d7e7f22358d4c86af3b
JOURDAIN Maximilien <mjourdain@p-212b-05.intra.gobelins.fr>
2016-04-21 10:40:45 +0200

    Fragments ok. Doing the grid

--- app/src/main/java/com/example/intervenant/myapplication/MainActivity.java
+++ app/src/main/java/com/example/intervenant/myapplication/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.intervenant.myapplication;
 
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+
+import com.example.intervenant.myapplication.com.example.intervenant.core.fragments.MViewFragment;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     ViewPager viewPager;
 
@@ -17,12 +22,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        viewPager = findViewById(R.id.pager);
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(new MPagerAdapter(getSupportFragmentManager()));
+
+        final TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
-    @Override
-    public void onClick(View view) {
+    public class MPagerAdapter extends FragmentPagerAdapter {
 
+        public MPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return MViewFragment.newInstance(position);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return position== 0 ? "All products" : "Cart";
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
     }
 
 
--- app/src/main/java/com/example/intervenant/myapplication/com/example/intervenant/core/ProductProvider.java
+++ app/src/main/java/com/example/intervenant/myapplication/com/example/intervenant/core/ProductProvider.java
@@ -1,6 +1,17 @@
 package com.example.intervenant.myapplication.com.example.intervenant.core;
 
-import com.example.intervenant.myapplication.R;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -8,5 +19,20 @@ import java.util.ArrayList;
  * Created by intervenant on 18/04/16.
  */
 public class ProductProvider {
+    public static void provideFromServer(final Context ctx, Response.Listener<JSONObject> cb) {
 
+
+        String url = "https://api.myjson.com/bins/27dd6";
+        RequestQueue mRequestQueue = Volley.newRequestQueue(ctx);
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest (Request.Method.GET, url, null,cb, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // TODO Auto-generated method stub
+                Toast.makeText(ctx, "Error", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        mRequestQueue.add(jsObjRequest);
+    }
 }
